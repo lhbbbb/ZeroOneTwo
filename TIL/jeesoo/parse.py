38be3533aba383e0c1c8c062145a401b490260c0
하지수 <haajeesoo@gmail.com>
2020-04-23 18:07:57 +0900

    feat: Update regular expression
     - add total price parsing
     - jira: S02P23A408-20

--- TIL/jeesoo/parse.py
+++ TIL/jeesoo/parse.py
@@ -2,7 +2,7 @@ import json
 import re
 from pprint import pprint
 
-with open('.\\examples\\02.json', 'rt', encoding='UTF8') as f:
+with open('.\\examples\\03.json', 'rt', encoding='UTF8') as f:
     content = json.load(f)
 
 min_font_sizes = []
@@ -27,25 +27,60 @@ pprint(texts)
 
 # 나라마다 다르게 처리...
 # 품목, 날짜
-r_items = r'[\d]+[.,][\d]*'
+r_items = r'[\d]+[.,][\d]*|TOTAL|TOTAAL'
 r_date = r'[\d]+[/:][\d]*'
-# r_test_1 = r'^\s*(\d+)\s+(.*\S)\s+'
+# if nation == US // 화폐단위는 나라 선택할 때 같이 고려
+r_dollar = r'[$]'
+# |WET TOTAL(NET TOTAL) 은 일단 제외
+r_total = r'^TOTAL|GRAND TOTAL|^TOTAAL'
+r_int = r'[\d]+'
+r_filters = r'TOTAL|SERVICE|CHANGE|MANAGER|^[\d.,\s]*$'
+# r_total = r'(?=(?!(SUB)).*)(?=((TOTAL))).*'
+# r_total = r'(?(?=(SUB))[^SUB]|.*)TOTAL'
+# r_total = r'(S|[^S].|SU|S[^U].|SUB|SU[^B])\sTOTAL'
+# r_total = r'(?!(SUB))\b(TOTAL)'
 title = ''
 items = []
 date = []
 for idx, text in enumerate(texts):
     if idx == 0:
         title = ' '.join(text)
-    if any(re.search(r_items, t) for t in text):
+    if any(re.search(r_items, t.upper()) for t in text):
         items.append(' '.join(text))
     if any(re.search(r_date, t) for t in text):
         date.append(' '.join(text))
-    # if any(re.search(r_test_1, t) for t in text):
-    #     print(text)
 
 print(title)
 pprint(items)
 pprint(date)
+
+# Total, TOTAL. Grand Total, Amount Due, Amount Paid
+# Total Due
+# Sub-Total
+# Total Owed
+
+for i in items:
+    if re.search(r_total, i.upper()):
+        total_strs = re.findall(r_int, i)
+        total_str = ''
+        for idx, s in enumerate(total_strs):
+            if idx == len(total_strs) - 1 and len(total_strs) != 1 and len(s) == 2:
+                total_str += f'.{s}'
+            else:
+                total_str += s
+    elif re.search(r_filters, i.upper()):
+        continue
+    else:
+        print(i)
+        price_strs = re.findall(r_int, i)
+        price_str = ''
+        for idx, s in enumerate(price_strs):
+            if idx == len(price_strs) - 1 and len(price_strs) != 1 and len(s) == 2:
+                price_str += f'.{s}'
+            else:
+                price_str += s
+        print(price_str)
+
 
 '''
 수량 품목 화폐단위 가격 
@@ -58,6 +93,11 @@ pprint(date)
 1. 데이터 보정 
 2. NLP 데이터 파싱 
 3. 정규표현식 
+'''
+
+'''
+US : $, '' // S, USD 
+JP : 
 '''
 
 # 주소, 전화번호

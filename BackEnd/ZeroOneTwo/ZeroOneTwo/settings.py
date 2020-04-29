459b777a7440fba4fc45097d4328756bb0e6f645
박진홍 <mpcato37@gmail.com>
2020-04-29 09:40:29 +0900

    feat: Change DB from MariaDB to SQLite

--- BackEnd/ZeroOneTwo/ZeroOneTwo/settings.py
+++ BackEnd/ZeroOneTwo/ZeroOneTwo/settings.py
@@ -86,14 +86,18 @@ WSGI_APPLICATION = 'ZeroOneTwo.wsgi.application'
 
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': config('DB_NAME'),
+    #     'USER': config('DB_USER'),
+    #     'PASSWORD': config('DB_PASSWORD'),
+    #     'HOST': config('DB_IP'),
+    #     'PORT': config('DB_PORT'),
+    #     'OPTIONS': {'charset': 'utf8mb4'}
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_IP'),
-        'PORT': config('DB_PORT'),
-        'OPTIONS': {'charset': 'utf8mb4'}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 

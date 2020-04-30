c982117ffcd1ad2ae062db8e523702a07bb1b123
박진홍 <mpcato37@gmail.com>
2020-04-30 22:32:38 +0900

    fix: bug fix at admin page not showing error

--- BackEnd/ZeroOneTwo/gateway/models.py
+++ BackEnd/ZeroOneTwo/gateway/models.py
@@ -22,9 +22,12 @@ class Boards(models.Model):
     startdate = models.DateField(blank=True, null=True)
     enddate = models.DateField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    register = models.ForeignKey('User', models.DO_NOTHING, db_column='register', related_name='board_register', blank=True, null=True)
+    register = models.ForeignKey('User', db_column='register', related_name='board_register', blank=True, null=True, on_delete=models.CASCADE)
     regdate = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     entry = models.ManyToManyField(User, related_name='boards', blank=True, null=True)
+    
+    class Meta:
+        ordering = ('-board_id',)
 
     def __str__(self):
         return self.title
@@ -39,7 +42,7 @@ class Items(models.Model):
     units = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
-        return self.origin_name
+        return str(self.origin_name)
 
 
 class Rate(models.Model):
@@ -59,9 +62,9 @@ class Receipts(models.Model):
     place = models.CharField(max_length=100, blank=True, null=True)
     address = models.CharField(max_length=200, blank=True, null=True)
     telephone = models.CharField(max_length=30, blank=True, null=True)
-    register = models.ForeignKey('User', models.DO_NOTHING, db_column='register', blank=True, null=True)
+    register = models.ForeignKey('User', db_column='register', blank=True, null=True, on_delete=models.CASCADE)
     regdate = models.DateTimeField(blank=True, null=True)
-    board = models.ForeignKey(Boards, models.DO_NOTHING, blank=True, null=True)
+    board = models.ForeignKey(Boards, blank=True, null=True, on_delete=models.CASCADE)
     country = models.CharField(max_length=50, blank=True, null=True)
     currency = models.CharField(max_length=20, blank=True, null=True)
     image = models.ImageField(blank=True, null=True, upload_to='images')
